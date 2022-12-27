b4ce3f6365751b7dcf6b588824403cb97221cd77
brupadiro <brunodiharce@gmail.com>
2022-12-27 18:29:29 -0300

    g

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    //baseURL: 'https://puntacable.webcreativos.com.uy/fullescabio/v1', 
+    baseURL: 'https://puntacable.webcreativos.com.uy/fullescabio/v1', 
+    //baseURL: 'http://localhost:3001/v1', 
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
