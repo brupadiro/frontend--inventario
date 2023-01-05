dc7ab894176fea51bf72d472e5eeea973918cf2d
brupadiro <brunodiharce@gmail.com>
2023-01-05 20:11:21 -0300

    url

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://puntacable.webcreativos.com.uy/fullescabio/v1',
+    baseURL: 'https://lionfish-app-niwhu.ondigitalocean.app/v1',
     //baseURL: 'http://localhost:3000/v1',
   },
 
