9029b90fa9db9b6304af96e7e61c3e5bb2a01041
brupadiro <brunodiharce@gmail.com>
2023-01-23 19:04:38 -0300

    cambios

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,8 +63,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    //baseURL: 'https://lionfish-app-niwhu.ondigitalocean.app/v1',
-    baseURL: 'http://localhost:3000/v1', 
+    baseURL: 'https://lionfish-app-niwhu.ondigitalocean.app/v1',
+    //baseURL: 'http://localhost:3000/v1', 
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
