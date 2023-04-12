163fb9eec707e1a53932d2d6ac0c7677021d80d3
brupadiro <brunodiharce@gmail.com>
2023-04-12 15:36:59 -0300

    G

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,10 +63,10 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://kennen.codiq.net/fullbackend/v1',
-    //baseURL: 'http://localhost:3000//v1', 
+    //baseURL: 'https://kennen.codiq.net/fullbackend/v1',
+    baseURL: 'http://localhost:3000/v1', 
   },
-
+ 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
