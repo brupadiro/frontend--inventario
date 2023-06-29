6573eb58bf78bbb68fc1245d25ad6d28a6fd8aea
brupadiro <brunodiharce@gmail.com>
2023-06-29 03:19:26 -0300

    cambiando url del backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://kennen.codiq.net/fullbackend/v1',
+    baseURL: 'https://kennen.descuentosya.uy/fullbackend/v1',
     //baseURL: 'http://localhost:3000/v1', 
   },
  
